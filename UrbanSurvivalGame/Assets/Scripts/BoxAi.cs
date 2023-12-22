a731f0fc6c06664deda58697c2f6ddd04d9ec562
GhaithBrunel <2157226@brunel.ac.uk>
2023-12-22 16:38:37 +0000

    Enhanced AI Behavior for Wolf
    - Implemented NavMesh-based patrolling with dynamic waypoint selection for more realistic wolf movement.
    - Added chasing behavior where the wolf speeds up when detecting the player within a specified chase radius.
    - Incorporated an attack mechanism allowing the wolf to damage the player when within attack range.
    - Introduced attack cooldown to prevent repeated instant attacks, enhancing gameplay balance.
    - Implemented logic for the wolf to face the player during attacks, creating more engaging and realistic interactions.
    - After attacking, the wolf now roams randomly around the player within a defined radius, adding to the complexity of the AI behavior.
    - Adjusted NavMeshAgent settings to respect player colliders, preventing the wolf from walking through the player model.
    - Serialized several parameters (patrol speed, chase speed, attack distance, etc.) for easy adjustments and fine-tuning from the Unity Editor.

--- UrbanSurvivalGame/Assets/Scripts/BoxAi.cs
+++ UrbanSurvivalGame/Assets/Scripts/BoxAi.cs
@@ -1,162 +1,119 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BoxAI : MonoBehaviour
 {
     public Transform player;
-    public float speed = 5f;
-    public float stopDistance = 1f;
-   [SerializeField] private float runAwayTimer;
-    private bool isRunningAway = false;
-    private Vector3 runAwayDirection;
-    private Rigidbody rb;
+    [SerializeField] private float patrolSpeed = 3.5f;
+    [SerializeField] private float chaseSpeed = 5f;
+    [SerializeField] private float attackDistance = 2f;
+    [SerializeField] private float attackMoveRadius = 5f;
+    [SerializeField] private float attackCooldown = 3f;
+    [SerializeField] private float waypointDistance = 20f;
+    [SerializeField] private float chaseRadius = 10f;
 
+    private float lastAttackTime;
+    private bool isAttacking = false;
 
-    
-    public Transform[] waypoints;
-    [SerializeField] public float chaseRadius = 10f;
-    [SerializeField] public float stopChaseTime = 10f;
+    private NavMeshAgent agent;
 
-    private Transform currentWaypoint;
-    [SerializeField] private float chaseTimer;
-    private bool isChasing;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        SelectNewWaypoint();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = patrolSpeed;
+        agent.stoppingDistance = attackDistance;
+        lastAttackTime = -attackCooldown;
+        SetNewDestination();
     }
 
     void Update()
     {
-        if (isRunningAway)
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= chaseRadius)
         {
-            RunAway();
-            if (runAwayTimer <= 0)
-            {
-                isRunningAway = false;
-                chaseTimer = stopChaseTime; // Reset chase timer to immediately resume chase
-            }
-        }
-        else if (Vector3.Distance(transform.position, player.position) <= chaseRadius)
-        {
-            ChasePlayer();
+            ChasePlayer(distanceToPlayer);
         }
         else
         {
-            Wander();
+            Patrol();
         }
 
-        CheckPlayerDistance();
+        if (isAttacking && distanceToPlayer > attackDistance)
+        {
+            isAttacking = false;
+        }
     }
 
-
-    void FollowPlayer()
+    void Patrol()
     {
-        float distance = Vector3.Distance(transform.position, player.position);
+        if (!agent.pathPending && agent.remainingDistance < 1f)
+        {
+            SetNewDestination();
+        }
+    }
+
+    void ChasePlayer(float distanceToPlayer)
+    {
+        agent.speed = chaseSpeed;
+
+        if (distanceToPlayer <= attackDistance && Time.time - lastAttackTime > attackCooldown)
+        {
+            AttackPlayer();
+        }
+        else if (!isAttacking)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    void AttackPlayer()
+    {
+        FacePlayer();
+        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(20);
+            lastAttackTime = Time.time;
+            isAttacking = true;
+            MoveRandomlyAroundPlayer();
+        }
+    }
+
+    void FacePlayer()
+    {
         Vector3 direction = (player.position - transform.position).normalized;
-        direction.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
 
-        if (direction != Vector3.zero && distance > stopDistance)
+    void MoveRandomlyAroundPlayer()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * attackMoveRadius;
+        randomDirection += player.position;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, attackMoveRadius, NavMesh.AllAreas))
         {
-            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
-            transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+            agent.SetDestination(hit.position);
         }
     }
 
-    void RunAway()
-{
-    if (runAwayTimer > 0)
+    void SetNewDestination()
     {
-        rb.MovePosition(rb.position + runAwayDirection * speed * Time.deltaTime);
-        runAwayTimer -= Time.deltaTime;
-        // Update wolf's rotation to face the run away direction
-        transform.rotation = Quaternion.LookRotation(runAwayDirection);
-    }
-    else
-    {
-        isRunningAway = false;
+        Vector3 randomDirection = Random.insideUnitSphere * waypointDistance;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, waypointDistance, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+        }
     }
 }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && !isRunningAway)
-        {
-            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                playerHealth.TakeDamage(20);
-                isRunningAway = true;
-                runAwayTimer = 3f; // Run away for 3 seconds after biting
-                runAwayDirection = Vector3.Normalize(transform.position - player.position);
-                runAwayDirection.y = 0;
-            }
-        }
-    }
-
-
-    void Wander()
-    {
-        if (Vector3.Distance(transform.position, currentWaypoint.position) < 1f)
-        {
-            SelectNewWaypoint();
-        }
-        else
-        {
-            MoveTowards(currentWaypoint.position);
-        }
-    }
-
-    void ChasePlayer()
-    {
-        if (Vector3.Distance(transform.position, player.position) <= chaseRadius)
-        {
-            chaseTimer = 0;
-            MoveTowards(player.position);
-        }
-        else
-        {
-            chaseTimer += Time.deltaTime;
-            if (chaseTimer >= stopChaseTime)
-            {
-                isChasing = false;
-                SelectNewWaypoint();
-            }
-        }
-
-
-        if (chaseTimer >= stopChaseTime)
-        {
-            isChasing = false;
-            isRunningAway = false; // Reset running away flag
-            SelectNewWaypoint();
-        }
-    }
-
-    void CheckPlayerDistance()
-    {
-        if (Vector3.Distance(transform.position, player.position) <= chaseRadius && !isRunningAway)
-        {
-            isChasing = true;
-        }
-    }
-
-    void SelectNewWaypoint()
-    {
-        if (waypoints.Length == 0) return;
-        currentWaypoint = waypoints[Random.Range(0, waypoints.Length)];
-        MoveTowards(currentWaypoint.position);
-    }
-
-    void MoveTowards(Vector3 target)
-    {
-        Vector3 direction = (target - transform.position).normalized;
-        rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
-        transform.LookAt(new Vector3(target.x, transform.position.y, target.z));
-    }
 
 
 
-}
+
 
 
 
